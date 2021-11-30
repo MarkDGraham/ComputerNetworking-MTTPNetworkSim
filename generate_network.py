3aece13e946630dc11a72417e603839653569f6a
Cooper Flourens <cooperflourens@gmail.com>
2021-11-30 14:50:11 -0800

    Creation and Initial Python
    Created CPP files and updated on generate_network and k_shortest_paths functions in python

---
+++ generate_network.py
@@ -0,0 +1,23 @@
+from itertools import islice
+import networkx as nx
+import matplotlib.pyplot as plt
+import random as rand
+
+#currently doesn't save the plot anywhere, just draws it.
+def generate_network(nodes, path = 'output.txt'):
+    G = nx.gnp_random_graph(nodes, .5)
+    # assign weights to edges
+    for (u, v) in G.edges():
+        G[u][v]['weight'] = rand.randint(1, 10)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels = True)
+    edge_labels = dict([((u, v,), d['weight'])
+                    for u, v, d in G.edges(data=True)])
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.show()
+
+#calculates the k shortest paths from node a to node B
+def k_shortest_paths(G, start, end, k, weight='weight'):
+    return list(islice(nx.shortest_simple_paths(G, start, end, weight=weight), k))
+
+
